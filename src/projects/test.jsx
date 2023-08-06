f55d70f4d712a1ebec0f2d65c8e64c43e92d6be1
King-Windows <krsazon@gmail.com>
2023-08-05 21:56:42 -0700

    test(#6)

--- src/projects/test.jsx
+++ src/projects/test.jsx
@@ -27,17 +27,31 @@ function book(title, author, pages, read){
   this.read = read;
 }
 
+book.prototype.info2 = function (){
+  console.log('wassup')
+}
+
+function book2(){
+}
+
+book2.prototype.info3 = function(){
+  console.log('wassup2')
+}
+
 console.log('----');
-book.prototype = Object.create(duplicateBookConstructor.prototype);
-// Object.setPrototypeOf(book.prototype, duplicateBookConstructor.prototype);
+// book.prototype = Object.create(duplicateBookConstructor.prototype);
+Object.setPrototypeOf(book.prototype, duplicateBookConstructor.prototype);
+Object.setPrototypeOf(book2.prototype, book.prototype);
 
 console.log(book.prototype);
 
-const authorSettingOne = new book('Magna Carta', 'Indra', 400, true);
-const authorSettingTwo = new book('Alkedama', 'Hanuman', 500, false);
+const authorSettingOne = new book2('Magna Carta', 'Indra', 400, true);
+const authorSettingTwo = new book2('Alkedama', 'Hanuman', 500, false);
 
 authorSettingOne.info();
 authorSettingTwo.info();
+authorSettingOne.info2();
+authorSettingOne.info3();
 
 // console.log(authorSettingOne.author);
 // console.log(authorSettingTwo.author);
